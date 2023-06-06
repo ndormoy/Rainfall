8bd03243067f7d38219915ab38e2368e40b54b92
Noe Dormoy <noedormoy@macbook-pro-de-noe-2.home>
2023-06-06 11:48:31 +0200

    Some fixes

--- level8/ressources/source_simplified.c
+++ level8/ressources/source_simplified.c
@@ -11,20 +11,16 @@ int main(void) {
 		printf("%p, %p\n", auth, service);
 		if (fgets(buffer, sizeof(buffer), stdin) == NULL)
 			break;
-		
 		if (strncmp(buffer, "auth ", 5) == 0) {
 			auth = malloc(4);
 			*auth = 0;
 			if (strlen(buffer + 5) <= 30)
 				strcpy(auth, buffer + 5);
 		}
-		
 		if (strncmp(buffer, "reset", 5) == 0)
 			free(auth);
-		
 		if (strncmp(buffer, "service", 6) == 0)
 			service = strdup(buffer + 7);
-		
 		if (strncmp(buffer, "login", 5) == 0) {
 			if (auth[32] != 0)
 				system("/bin/sh");
@@ -32,6 +28,5 @@ int main(void) {
 				fwrite("Password:\n", 1, 10, stdout);
 		}
 	}
-	
 	return 0;
 }
